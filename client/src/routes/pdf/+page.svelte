d89cce4c23798dcb05fdb1e44bf57f67a5634f2f
Graham Mainwaring <graham@mhn.org>
2025-09-10 01:39:00 -0400

    Add UI formatting & fix linter issues

--- client/src/routes/pdf/+page.svelte
+++ client/src/routes/pdf/+page.svelte
@@ -1,45 +1,46 @@
 <script lang="ts">
-  import { onMount } from 'svelte';
+	import { onMount } from 'svelte';
+	import { notifier } from '@beyonk/svelte-notifications';
 
-  let data = null;
-  let PdfViewer;
+	let data = null;
+	let PdfViewer;
 
-  onMount(async () => {
-    fetchFile('private/8d2e9806-154b-4461-bd14-078ab3e6fa6b.pdf');
-    const module = await import("svelte-pdf");
-    PdfViewer = module.default;
-  });
+	onMount(async () => {
+		fetchFile('private/8d2e9806-154b-4461-bd14-078ab3e6fa6b.pdf');
+		const module = await import('svelte-pdf');
+		PdfViewer = module.default;
+	});
 
-  const fetchFile = async (name) => {
-    var json = JSON.stringify({
-      name: name
-    });
-    const response = await fetch('https://groundschool.wingsofcarolina.org/api/fetch', {
-      method: "post",
-      withCredentials: true,
-      headers: {
-        'Accept': 'application/pdf',
-        'Content-Type': 'application/json'
-      },
-      body: json
-    });
-    if (!response.ok) {
-      notifier.danger('Retrieve of requested document failed.');
-    } else {
-      data = await response.arrayBuffer();
-    }
-  }
+	const fetchFile = async (name) => {
+		var json = JSON.stringify({
+			name: name
+		});
+		const response = await fetch('https://groundschool.wingsofcarolina.org/api/fetch', {
+			method: 'post',
+			credentials: 'include',
+			headers: {
+				Accept: 'application/pdf',
+				'Content-Type': 'application/json'
+			},
+			body: json
+		});
+		if (!response.ok) {
+			notifier.danger('Retrieve of requested document failed.');
+		} else {
+			data = await response.arrayBuffer();
+		}
+	};
 </script>
 
 <div class="center margins">
-  {#if data}
-  <svelte:component this={PdfViewer} data={data} />
-  {/if}
+	{#if data}
+		<svelte:component this={PdfViewer} {data} />
+	{/if}
 </div>
 
 <style>
-.margins {
-  margin: 20px;
-  margin-left: 100px;
-}
+	.margins {
+		margin: 20px;
+		margin-left: 100px;
+	}
 </style>
